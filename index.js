d2a3a15ad7d680491a3dc282aef4f51d012d174a
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-28 16:21:08 +0000

    Add missing export

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const rand = {
+export const rand = {
   alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 
   getNumber: (min, max) => Math.floor(Math.random() * (max - min + 1) + min),
-  
+
   getChar: function () {
     return this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length))
   }
