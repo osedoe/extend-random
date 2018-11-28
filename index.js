67e9da15efa1f9bd8e7d0138a1d2ab6f115eccc0
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2018-11-28 15:30:27 +0000

    Change function name

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const rand = {
   alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
 
   getNumber: (min, max) => Math.floor(Math.random() * (max - min + 1) + min),
-  getLetter: function () {
+  
+  getChar: function () {
     return this.alphabet.charAt(Math.floor(Math.random() * this.alphabet.length))
   }
 };
