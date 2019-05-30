c13cb3d8688c87336fa06bb0d401ccfe98f0162e
Jose Diaz-Gonzalez <jose.diazg@protonmail.com>
2019-05-30 12:13:48 +0100

    Update function names and add more files to gitignore

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-export const getNumber = (min: number, max: number): number => {
+export const generateRandomNumber = (min: number, max: number): number => {
   return Math.floor(Math.random() * (max - min + 1) + min)
 };
 
-export const getChar = (): string => {
+export const generateRandomChar = (): string => {
   const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   return alphabet.charAt(Math.floor(Math.random() * alphabet.length))
 };
